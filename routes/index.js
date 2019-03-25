18624a05118c49f750252b443e39ccd5f29c25a7
verdantpath <m_dittmann@yahoo.com>
2019-03-24 20:26:08 -0700

    sessions, cookies, authenticate user

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,66 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/users');
 
+// GET /profile
+router.get('/profile', function(req, res, next) {
+  if (! req.session.userId ) {
+    var err = new Error("You are not authorized to view this page.");
+    err.status = 403;
+    return next(err);
+  }
+  User.findById(req.session.userId)
+      .exec(function (err, user) {
+        if (err) {
+          return next(error);
+        } else {
+          return res.render('profile', {
+            title: 'Profile',
+            name: user.name,
+            favorite: user.favoriteBook
+          });
+        }
+      });
+});
+
+// GET /logout
+router.get('/logout', function(req, res, next) {
+  if (req.session) {
+    // delete session object
+    req.session.destroy(function(err) {
+      if(err) {
+        return next(err);
+      } else {
+        return res.redirect('/');
+      }
+    });
+  }
+});
+
+// GET /login
+router.get('/login', function(req, res, next) {
+  return res.render('login', { title: 'Log In'});
+})
+
+// POST /login
+router.post('/login', function(req, res, next) {
+  if (req.body.email && req.body.password) {
+    User.authenticate(req.body.email, req.body.password, function(error, user) {
+      if (error || !user) {
+        var err = new Error('Wrong email or password.');
+        err.status = 401;
+        return next(err);
+      } else {
+        req.session.userId = user._id;
+        return res.redirect('/profile');
+      }
+    });
+  } else {
+    var err = new Error('Email and password are required.');
+    err.status = 401;
+    return next(err);
+  }
+});
+
 // GET /register
 router.get('/register', function(req, res, next) {
   return res.render('register', { title: 'Sign Up' });
@@ -10,10 +70,10 @@ router.get('/register', function(req, res, next) {
 // POST /register
 router.post('/register', function(req, res, next) {
   if (req.body.email &&
-    req.body.name &&
-    req.body.favoriteBook &&
-    req.body.password &&
-    req.body.confirmPassword) {
+      req.body.name &&
+      req.body.favoriteBook &&
+      req.body.password &&
+      req.body.confirmPassword) {
 
       // confirm that user typed same password twice
       if (req.body.password !== req.body.confirmPassword) {
@@ -35,6 +95,7 @@ router.post('/register', function(req, res, next) {
         if (error) {
           return next(error);
         } else {
+          req.session.userId = user._id;
           return res.redirect('/profile');
         }
       });
